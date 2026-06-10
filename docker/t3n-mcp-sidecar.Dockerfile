nearai/ironclaw

Integrate t3n mcp to have context on onboarding

docker/t3n-mcp-sidecar.Dockerfile
@@ -1,0 +1,49 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+RUN apk add --no-cache dumb-init \
+    && npm install --global pnpm
+
+ARG GITHUB_TOKEN
+
+COPY trinity/client/mcp/t3n-mcp/package.json ./package.json
+COPY trinity/client/mcp/t3n-mcp/pnpm-lock.yaml ./pnpm-lock.yaml
+
+RUN if [ -n "$GITHUB_TOKEN" ]; then \
+      echo "@terminal-3:registry=https://npm.pkg.github.com" > .npmrc && \
+      echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" >> .npmrc; \
+    fi
+
+RUN pnpm install --frozen-lockfile && rm -f .npmrc
+
+COPY trinity/client/mcp/t3n-mcp/src ./src
+COPY trinity/client/mcp/t3n-mcp/bin ./bin
+COPY trinity/client/mcp/t3n-mcp/tsconfig.json ./tsconfig.json
+COPY trinity/client/mcp/t3n-mcp/tsconfig.prod.json ./tsconfig.prod.json
+COPY trinity/client/mcp/t3n-mcp/config.json ./config.json
+COPY trinity/client/mcp/t3n-mcp/config.production.json ./config.production.json
+COPY trinity/client/mcp/t3n-mcp/config.staging.json ./config.staging.json
+COPY trinity/client/mcp/t3n-mcp/config.local.json ./config.local.json
+COPY trinity/client/shared/bin ./shared/bin
+COPY bastion-claw/docker/t3n-mcp-bridge.mjs /bridge/t3n-mcp-bridge.mjs
+
+RUN mkdir -p /shared && cp -r /app/shared/bin /shared/bin
+
+RUN pnpm run build
+
+RUN mkdir -p ../.. \
+    && ln -s /app/shared ../../shared \
+    && addgroup -S t3n \
+    && adduser -S -G t3n t3n \
+    && chown -R t3n:t3n /app /bridge
+
+USER t3n
+
+ENV NODE_ENV=production
+ENV LOG_LEVEL=info
+ENV T3N_PROJECT_DIR=/app
+ENV MCP_SOCKET_PATH=/var/run/t3n-mcp/t3n-mcp.sock
+
+ENTRYPOINT ["dumb-init", "--"]
+CMD ["node", "/bridge/t3n-mcp-bridge.mjs"]

src/cli/mcp.rs
@@ -10,9 +10,10 @@ use clap::{Args, Subcommand};
 
 use crate::config::Config;
 use crate::db::Database;
-use crate::secrets::SecretsStore;
+use crate::secrets::{CreateSecretParams, SecretsStore};
 use crate::tools::mcp::{
-    McpClient, McpProcessManager, McpServerConfig, McpSessionManager, OAuthConfig,
+    LocalMcpAuthConfig, McpClient, McpProcessManager, McpServerConfig, McpSessionManager,
+    OAuthConfig,
     auth::{authorize_mcp_server, is_authenticated},
     config::{self, EffectiveTransport, McpServersFile},
     factory::create_client_from_config,
@@ -70,6 +71,14 @@ pub struct McpAddArgs {
     /// Server description
     #[arg(long)]
     pub description: Option<String>,
+
+    /// Browser URL for a one-time local setup step on stdio/unix transports.
+    #[arg(long)]
+    pub local_auth_url: Option<String>,
+
+    /// Instructions shown when users must complete a one-time local setup step.
+    #[arg(long)]
+    pub local_auth_instructions: Option<String>,
 }
 
 #[derive(Subcommand, Debug, Clone)]
@@ -139,6 +148,25 @@ fn parse_env_var(s: &str) -> Result<(String, String), String> {
     Ok((s[..pos].to_string(), s[pos + 1..].to_string()))
 }
 
+const T3N_MCP_NAME: &str = "t3n-mcp";
+const T3N_LOGIN_URL: &str = "https://staging.network.terminal3.io/login";
+const T3N_LOCAL_AUTH_INSTRUCTIONS: &str = "Sign in to Trinity or create your account, then grant the agent permission to manage your Trinity profile and data.";
+
+fn builtin_local_auth_defaults(name: &str, transport: &str) -> Option<LocalMcpAuthConfig> {
+    let is_local_transport = matches!(transport, "stdio" | "unix");
+    if is_local_transport && name.eq_ignore_ascii_case(T3N_MCP_NAME) {
+        Some(
+            LocalMcpAuthConfig::new(T3N_LOCAL_AUTH_INSTRUCTIONS)
+                .with_auth_url(T3N_LOGIN_URL)
+                .with_completion_message(
+                    "Trinity setup confirmed. BastionClaw can now use this MCP server.",
+                ),
+        )
+    } else {
+        None
+    }
+}
+
 /// Run an MCP command.
 pub async fn run_mcp_command(cmd: McpCommand) -> anyhow::Result<()> {
     match cmd {
@@ -179,6 +207,8 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
         token_url,
         scopes,
         description,
+        local_auth_url,
+        local_auth_instructions,
     } = args;
 
     let transport_lower = transport.to_lowercase();
@@ -221,6 +251,26 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
         config = config.with_description(desc);
     }
 
+    let local_auth = if local_auth_url.is_some() || local_auth_instructions.is_some() {
+        let instructions = local_auth_instructions.unwrap_or_else(|| {
+            format!(
+                "Complete the required setup for '{}' before BastionClaw uses this MCP server.",
+                name
+            )
+        });
+        let local_auth = LocalMcpAuthConfig::new(instructions);
+        Some(if let Some(url) = local_auth_url {
+            local_auth.with_auth_url(url)
+        } else {
+            local_auth
+        })
+    } else {
+        builtin_local_auth_defaults(&name, &transport_lower)
+    };
+    if let Some(local_auth) = local_auth {
+        config = config.with_local_auth(local_auth);
+    }
+
     // Track if auth is required
     let requires_auth = client_id.is_some();
 
@@ -250,6 +300,7 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
     // Validate
     config.validate()?;
     let has_custom_auth_header = config.has_custom_auth_header();
+    let has_local_auth = config.local_auth.is_some();
 
     // Save (DB if available, else disk)
     let (db, owner_id) = connect_db().await;
@@ -281,6 +332,12 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
     if requires_auth && !has_custom_auth_header {
         println!();
         println!("  Run 'bastionclaw mcp auth {}' to authenticate.", name);
+    }
+    if has_local_auth {
+        println!();
+        println!(
+            "  This server has a one-time local setup step. BastionClaw will direct users to the configured page when access is needed."
+        );
     }
 
     println!();
@@ -432,6 +489,82 @@ async fn auth_server(name: String, user_id: String) -> anyhow::Result<()> {
 
     // Initialize secrets store
     let secrets = get_secrets_store().await?;
+
+    if server.is_local_transport() {
+        if let Some(local_auth) = &server.local_auth {
+            if let Some(secret_name) = server.local_auth_completion_secret_name()
+                && secrets
+                    .exists(&user_id, &secret_name)
+                    .await
+                    .unwrap_or(false)
+            {
+                println!();
+                println!(
+                    "  Server '{}' is already marked ready for '{}'.",
+                    name, user_id
+                );
+                println!();
+                return Ok(());
+            }
+
+            println!();
+            println!("╔════════════════════════════════════════════════════════════════╗");
+            println!("║  {:^62}║", format!("{} Setup", name.to_uppercase()));
+            println!("╚════════════════════════════════════════════════════════════════╝");
+            println!();
+            println!("  {}", local_auth.instructions.trim());
+            if let Some(url) = &local_auth.auth_url {
+                println!();
+                println!("  Open:");
+                println!("    {}", url);
+            }
+            println!();
+            print!("  Type 'done' when this has been completed: ");
+            std::io::stdout().flush()?;
+
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input)?;
+            let normalized = input.trim().to_ascii_lowercase();
+            if !matches!(
+                normalized.as_str(),
+                "done" | "ready" | "complete" | "completed" | "authorised" | "authorized"
+            ) {
+                anyhow::bail!(
+                    "Setup not confirmed. Complete the external step, then run 'bastionclaw mcp auth {}' again.",
+                    name
+                );
+            }
+
+            let secret_name = server
+                .local_auth_completion_secret_name()
+                .ok_or_else(|| anyhow::anyhow!("Local auth marker is not configured"))?;
+            secrets
+                .create(
+                    &user_id,
+                    CreateSecretParams::new(&secret_name, "confirmed").with_provider(name.clone()),
+                )
+                .await?;
+
+            println!();
+            println!(
+                "  ✓ {}",
+                local_auth
+                    .completion_message
+                    .as_deref()
+                    .unwrap_or("Setup confirmed. BastionClaw can now use this MCP server.")
+            );
+            println!();
+            return Ok(());
+        }
+
+        println!();
+        println!(
+            "  Server '{}' uses a local transport and does not require MCP authentication.",
+            name
+        );
+        println!();
+        return Ok(());
+    }
 
     // Check if already authenticated
     if is_authenticated(&server, &secrets, &user_id).await {
@@ -740,5 +873,31 @@ mod tests {
         let result = parse_env_var("no-equals-here");
         assert!(result.is_err());
         assert!(result.unwrap_err().contains("invalid env var format"));
+    }
+
+    #[test]
+    fn test_builtin_local_auth_defaults_seed_trinity_guidance_for_local_transport() {
+        let local_auth = builtin_local_auth_defaults("t3n-mcp", "unix").expect("local_auth");
+        assert_eq!(
+            local_auth.auth_url.as_deref(),
+            Some("https://staging.network.terminal3.io/login")
+        );
+        assert!(
+            local_auth
+                .instructions
+                .contains("grant the agent permission"),
+            "expected Trinity permission guidance, got: {}",
+            local_auth.instructions
+        );
+        assert_eq!(
+            local_auth.completion_message.as_deref(),
+            Some("Trinity setup confirmed. BastionClaw can now use this MCP server.")
+        );
+    }
+
+    #[test]
+    fn test_builtin_local_auth_defaults_skip_http_transport() {
+        assert!(builtin_local_auth_defaults("t3n-mcp", "http").is_none());
+        assert!(builtin_local_auth_defaults("other-server", "unix").is_none());
     }
 }

Terminal-3/bastion-claw

(cherry picked from commit 9767ff6c06f1b01016940bac5614825cd8cb6b18)
[rebase-port: renamed bastionclaw->t3claw throughout; dropped .env.example
t3n section and fork CHANGELOG entry (both absent from staging final state);
dropped old-rename formatting-only hunks already covered by the t3claw
rename pass]

docker/t3n-mcp-sidecar.Dockerfile
@@ -1,0 +1,49 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+RUN apk add --no-cache dumb-init \
+    && npm install --global pnpm
+
+ARG GITHUB_TOKEN
+
+COPY trinity/client/mcp/t3n-mcp/package.json ./package.json
+COPY trinity/client/mcp/t3n-mcp/pnpm-lock.yaml ./pnpm-lock.yaml
+
+RUN if [ -n "$GITHUB_TOKEN" ]; then \
+      echo "@terminal-3:registry=https://npm.pkg.github.com" > .npmrc && \
+      echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" >> .npmrc; \
+    fi
+
+RUN pnpm install --frozen-lockfile && rm -f .npmrc
+
+COPY trinity/client/mcp/t3n-mcp/src ./src
+COPY trinity/client/mcp/t3n-mcp/bin ./bin
+COPY trinity/client/mcp/t3n-mcp/tsconfig.json ./tsconfig.json
+COPY trinity/client/mcp/t3n-mcp/tsconfig.prod.json ./tsconfig.prod.json
+COPY trinity/client/mcp/t3n-mcp/config.json ./config.json
+COPY trinity/client/mcp/t3n-mcp/config.production.json ./config.production.json
+COPY trinity/client/mcp/t3n-mcp/config.staging.json ./config.staging.json
+COPY trinity/client/mcp/t3n-mcp/config.local.json ./config.local.json
+COPY trinity/client/shared/bin ./shared/bin
+COPY t3-claw/docker/t3n-mcp-bridge.mjs /bridge/t3n-mcp-bridge.mjs
+
+RUN mkdir -p /shared && cp -r /app/shared/bin /shared/bin
+
+RUN pnpm run build
+
+RUN mkdir -p ../.. \
+    && ln -s /app/shared ../../shared \
+    && addgroup -S t3n \
+    && adduser -S -G t3n t3n \
+    && chown -R t3n:t3n /app /bridge
+
+USER t3n
+
+ENV NODE_ENV=production
+ENV LOG_LEVEL=info
+ENV T3N_PROJECT_DIR=/app
+ENV MCP_SOCKET_PATH=/var/run/t3n-mcp/t3n-mcp.sock
+
+ENTRYPOINT ["dumb-init", "--"]
+CMD ["node", "/bridge/t3n-mcp-bridge.mjs"]

src/cli/mcp.rs
@@ -10,9 +10,10 @@ use clap::{Args, Subcommand};
 
 use crate::config::Config;
 use crate::db::Database;
-use crate::secrets::SecretsStore;
+use crate::secrets::{CreateSecretParams, SecretsStore};
 use crate::tools::mcp::{
-    McpClient, McpProcessManager, McpServerConfig, McpSessionManager, OAuthConfig,
+    LocalMcpAuthConfig, McpClient, McpProcessManager, McpServerConfig, McpSessionManager,
+    OAuthConfig,
     auth::{authorize_mcp_server, is_authenticated},
     config::{self, EffectiveTransport, McpServersFile},
     factory::create_client_from_config,
@@ -70,6 +71,14 @@ pub struct McpAddArgs {
     /// Server description
     #[arg(long)]
     pub description: Option<String>,
+
+    /// Browser URL for a one-time local setup step on stdio/unix transports.
+    #[arg(long)]
+    pub local_auth_url: Option<String>,
+
+    /// Instructions shown when users must complete a one-time local setup step.
+    #[arg(long)]
+    pub local_auth_instructions: Option<String>,
 }
 
 #[derive(Subcommand, Debug, Clone)]
@@ -139,6 +148,25 @@ fn parse_env_var(s: &str) -> Result<(String, String), String> {
     Ok((s[..pos].to_string(), s[pos + 1..].to_string()))
 }
 
+const T3N_MCP_NAME: &str = "t3n-mcp";
+const T3N_LOGIN_URL: &str = "https://staging.network.terminal3.io/login";
+const T3N_LOCAL_AUTH_INSTRUCTIONS: &str = "Sign in to Trinity or create your account, then grant the agent permission to manage your Trinity profile and data.";
+
+fn builtin_local_auth_defaults(name: &str, transport: &str) -> Option<LocalMcpAuthConfig> {
+    let is_local_transport = matches!(transport, "stdio" | "unix");
+    if is_local_transport && name.eq_ignore_ascii_case(T3N_MCP_NAME) {
+        Some(
+            LocalMcpAuthConfig::new(T3N_LOCAL_AUTH_INSTRUCTIONS)
+                .with_auth_url(T3N_LOGIN_URL)
+                .with_completion_message(
+                    "Trinity setup confirmed. T3Claw can now use this MCP server.",
+                ),
+        )
+    } else {
+        None
+    }
+}
+
 /// Run an MCP command.
 pub async fn run_mcp_command(cmd: McpCommand) -> anyhow::Result<()> {
     match cmd {
@@ -179,6 +207,8 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
         token_url,
         scopes,
         description,
+        local_auth_url,
+        local_auth_instructions,
     } = args;
 
     let transport_lower = transport.to_lowercase();
@@ -221,6 +251,26 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
         config = config.with_description(desc);
     }
 
+    let local_auth = if local_auth_url.is_some() || local_auth_instructions.is_some() {
+        let instructions = local_auth_instructions.unwrap_or_else(|| {
+            format!(
+                "Complete the required setup for '{}' before T3Claw uses this MCP server.",
+                name
+            )
+        });
+        let local_auth = LocalMcpAuthConfig::new(instructions);
+        Some(if let Some(url) = local_auth_url {
+            local_auth.with_auth_url(url)
+        } else {
+            local_auth
+        })
+    } else {
+        builtin_local_auth_defaults(&name, &transport_lower)
+    };
+    if let Some(local_auth) = local_auth {
+        config = config.with_local_auth(local_auth);
+    }
+
     // Track if auth is required
     let requires_auth = client_id.is_some();
 
@@ -250,6 +300,7 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
     // Validate
     config.validate()?;
     let has_custom_auth_header = config.has_custom_auth_header();
+    let has_local_auth = config.local_auth.is_some();
 
     // Save (DB if available, else disk)
     let (db, owner_id) = connect_db().await;
@@ -281,6 +332,12 @@ async fn add_server(args: McpAddArgs) -> anyhow::Result<()> {
     if requires_auth && !has_custom_auth_header {
         println!();
         println!("  Run 't3claw mcp auth {}' to authenticate.", name);
+    }
+    if has_local_auth {
+        println!();
+        println!(
+            "  This server has a one-time local setup step. T3Claw will direct users to the configured page when access is needed."
+        );
     }
 
     println!();
@@ -432,6 +489,82 @@ async fn auth_server(name: String, user_id: String) -> anyhow::Result<()> {
 
     // Initialize secrets store
     let secrets = get_secrets_store().await?;
+
+    if server.is_local_transport() {
+        if let Some(local_auth) = &server.local_auth {
+            if let Some(secret_name) = server.local_auth_completion_secret_name()
+                && secrets
+                    .exists(&user_id, &secret_name)
+                    .await
+                    .unwrap_or(false)
+            {
+                println!();
+                println!(
+                    "  Server '{}' is already marked ready for '{}'.",
+                    name, user_id
+                );
+                println!();
+                return Ok(());
+            }
+
+            println!();
+            println!("╔════════════════════════════════════════════════════════════════╗");
+            println!("║  {:^62}║", format!("{} Setup", name.to_uppercase()));
+            println!("╚════════════════════════════════════════════════════════════════╝");
+            println!();
+            println!("  {}", local_auth.instructions.trim());
+            if let Some(url) = &local_auth.auth_url {
+                println!();
+                println!("  Open:");
+                println!("    {}", url);
+            }
+            println!();
+            print!("  Type 'done' when this has been completed: ");
+            std::io::stdout().flush()?;
+
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input)?;
+            let normalized = input.trim().to_ascii_lowercase();
+            if !matches!(
+                normalized.as_str(),
+                "done" | "ready" | "complete" | "completed" | "authorised" | "authorized"
+            ) {
+                anyhow::bail!(
+                    "Setup not confirmed. Complete the external step, then run 't3claw mcp auth {}' again.",
+                    name
+                );
+            }
+
+            let secret_name = server
+                .local_auth_completion_secret_name()
+                .ok_or_else(|| anyhow::anyhow!("Local auth marker is not configured"))?;
+            secrets
+                .create(
+                    &user_id,
+                    CreateSecretParams::new(&secret_name, "confirmed").with_provider(name.clone()),
+                )
+                .await?;
+
+            println!();
+            println!(
+                "  ✓ {}",
+                local_auth
+                    .completion_message
+                    .as_deref()
+                    .unwrap_or("Setup confirmed. T3Claw can now use this MCP server.")
+            );
+            println!();
+            return Ok(());
+        }
+
+        println!();
+        println!(
+            "  Server '{}' uses a local transport and does not require MCP authentication.",
+            name
+        );
+        println!();
+        return Ok(());
+    }
 
     // Check if already authenticated
     if is_authenticated(&server, &secrets, &user_id).await {
@@ -788,5 +921,31 @@ mod tests {
         assert!(mixed.len() > 60);
         let truncated = truncate_description(&mixed);
         assert!(truncated.ends_with("..."));
+    }
+
+    #[test]
+    fn test_builtin_local_auth_defaults_seed_trinity_guidance_for_local_transport() {
+        let local_auth = builtin_local_auth_defaults("t3n-mcp", "unix").expect("local_auth");
+        assert_eq!(
+            local_auth.auth_url.as_deref(),
+            Some("https://staging.network.terminal3.io/login")
+        );
+        assert!(
+            local_auth
+                .instructions
+                .contains("grant the agent permission"),
+            "expected Trinity permission guidance, got: {}",
+            local_auth.instructions
+        );
+        assert_eq!(
+            local_auth.completion_message.as_deref(),
+            Some("Trinity setup confirmed. T3Claw can now use this MCP server.")
+        );
+    }
+
+    #[test]
+    fn test_builtin_local_auth_defaults_skip_http_transport() {
+        assert!(builtin_local_auth_defaults("t3n-mcp", "http").is_none());
+        assert!(builtin_local_auth_defaults("other-server", "unix").is_none());
     }
 }